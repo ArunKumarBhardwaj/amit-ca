859856ede6fcb1a2e1eaea501798b2f530a79201
ArunKumarBhardwaj <bhardwajarun38@gmail.com>
2024-08-25 14:55:46 +0530

    border property error fix

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -6,11 +6,9 @@ const Map = () => {
       <iframe
         width="100%"
         height="400"
-        frameborder="0"
-        style={{ border: 0 }}
-        allowfullscreen=""
+        style={{ border: "0px" }}
+        allow="fullscreen"
         aria-hidden="false"
-        tabindex="0"
         src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=210,%20Neelkanth%20Complex,%20Rajpur%20Road,%20Dehradun-248001+(Jakwal%20Advisors)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       ></iframe>
     </div>
